67ef571570a05cb191469129bcb3303ca4ed71e8
Zornitsa Dineva <zornitsa_angelova@yahoo.com>
2021-03-09 23:45:02 +0200

    finish vehicles

--- js/app.js
+++ js/app.js
@@ -106,19 +106,75 @@ function loadCustomers() {
 
 /* CRUD FOR VEHICLE */
 
-//TODO
+function getVehicles() {
+    return getAll(VEHICLE);
+}
+
+function getVehicle(id) {
+    return getById(VEHICLE, id);
+}
+
+function saveVehicles() {
+    saveAll(VEHICLE);
+}
+
+function addNewVehicle(vehicle) {
+    addNewObj(VEHICLE, vehicle);
+}
+
+function updateVehicle(vehicle) {
+    updateObj(VEHICLE, vehicle);
+}
+
+function deleteVehicle(id) {
+    deleteObj(VEHICLE, id)
+}
+
+function loadVehicles() {
+    loadAll(VEHICLE);
+}
 
 /* CRUD FOR RENTS */
 
-//TODO
+function getRents() {
+    return getAll(RENT);
+}
+
+function getRent(id) {
+    return getById(RENT, id);
+}
+
+function saveRents() {
+    saveAll(RENT);
+}
+
+function addNewRent(rent) {
+    addNewObj(RENT, rent);
+}
+
+function updateRent(rent) {
+    updateObj(RENT, rent);
+}
+
+function deleteRent(id) {
+    deleteObj(RENT, id)
+}
+
+function loadRents() {
+    loadAll(RENT);
+}
 
 /* Business Logic */
 
 function loadDB() {
     loadCustomers();
+    loadVehicles();
+    loadRents();
 }
 
 loadDB();
+
+/* Business Logic CUSTOMER */
 
 function bindCustomerAddFormSubmission() {
     var $form = $('form#add-customer');
@@ -141,7 +197,6 @@ bindCustomerAddFormSubmission();
 
 function getCustomersHtml() {
     var html = '';
-
     var index = 0;
 
     for (var customer of getCustomers()) {
@@ -230,11 +285,143 @@ function bindCustomerUpdateFormSubmission() {
     })
 }
 
+/* Business Logic VEHICLE */
+
+function bindVehicleAddFormSubmission() {
+    var $form = $('form#add-vehicle');
+    $form.on('submit', function(e) {
+        e.preventDefault();
+
+        var vehicle = {};
+        vehicle.brand = $form.find('input#brand').val();
+        vehicle.model = $form.find('input#model').val();
+        vehicle.year = $form.find('input#year').val();
+        vehicle.fuel = $form.find('input#fuel').val();
+        vehicle.seats = $form.find('input#seats').val();
+        vehicle.price = $form.find('input#price').val();
+
+        if (vehicle.brand && vehicle.model && vehicle.year 
+            && vehicle.fuel && vehicle.seats && vehicle.price) {
+            addNewVehicle(vehicle);
+            refresh();  
+        }
+    })
+}
+
+bindVehicleAddFormSubmission();
+
+function getVehiclesHtml() {
+    var html = '';
+    var index = 0;
+
+    for (var vehicle of getVehicles()) {
+        index = index + 1;
+        html = html + getVehicleHtml(vehicle, index);
+    }
+
+    return html;
+}
+
+function getVehicleHtml(vehicle, index) {
+    var html = `
+        <tr data-id="${vehicle.id}">
+            <td>${index}</td>
+            <td>${vehicle.brand}</td>
+            <td>${vehicle.model}</td>
+            <td>${vehicle.year}</td>
+            <td>${vehicle.fuel}</td>
+            <td>${vehicle.seats}</td>
+            <td><img src="images/car.jpg" width="25" height="25"></td>
+            <td>${vehicle.price}</td>
+            <td></td>
+            <td>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default update-vehicle">Update</button>
+                    <button type="button" class="btn btn-default delete-vehicle">Delete</button>
+                </div>
+            </td>
+        </tr>
+    `;
+    return html;
+}
+
+function showVehicles() {
+    var html = getVehiclesHtml();
+    $('table#vehicles tbody').html(html);
+}
+
+function bindVehicleDeleteButtons() {
+    $('button.delete-vehicle').on('click', function(e){
+        $this = $(this);
+        var id = $this.closest('tr').data('id');
+        deleteVehicle(id);
+        refresh();
+    });
+}
+
+function bindVehicleUpdateButtons() {
+    $('button.update-vehicle').on('click', function(e){
+        $this = $(this);
+        var id = $this.closest('tr').data('id');
+        var vehicle = getVehicle(id);
+        var $form = $('form#update-vehicle');
+        $form.find('input#id').val(vehicle.id);
+        $form.find('input#brand').val(vehicle.brand);
+        $form.find('input#model').val(vehicle.model);
+        $form.find('input#year').val(vehicle.year);
+        $form.find('input#fuel').val(vehicle.fuel);
+        $form.find('input#seats').val(vehicle.seats);
+        $form.find('input#price').val(vehicle.price);
+        $form.show();
+
+        // scroll to the form
+        $('body, html').animate(
+            {
+              scrollTop: $('form#update-vehicle').offset().top
+            },
+            800 //speed
+        );
+        bindVehicleUpdateFormSubmission();
+    });
+}
+
+function hideVehicleUpdateForm() {
+    var $form = $('form#update-vehicle');
+    $form.hide();
+}
+
+function bindVehicleUpdateFormSubmission() {
+    var $form = $('form#update-vehicle');
+    $form.on('submit', function(e) {
+        e.preventDefault();
+
+        var vehicle = {};
+        vehicle.id = $form.find('input#id').val();
+        vehicle.brand = $form.find('input#brand').val();
+        vehicle.model = $form.find('input#model').val();
+        vehicle.year = $form.find('input#year').val();
+        vehicle.fuel = $form.find('input#fuel').val();
+        vehicle.seats = $form.find('input#seats').val();
+        vehicle.price = $form.find('input#price').val();
+
+        if (vehicle.brand && vehicle.model && vehicle.year 
+            && vehicle.fuel && vehicle.seats && vehicle.price) {
+            updateVehicle(vehicle);
+            refresh();  
+        }
+    })
+}
+
 function refresh() {
     showCustomers();
     bindCustomerDeleteButtons();
     bindCustomerUpdateButtons();
     hideCustomerUpdateForm();
+
+    showVehicles();
+    bindVehicleDeleteButtons();
+    bindVehicleUpdateButtons();
+    hideVehicleUpdateForm();
 }
 
 refresh();
